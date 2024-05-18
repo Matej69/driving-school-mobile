77a9a8efa7de95ff8c050c2139997f2691c11862
Ja <valentinoromansi@gmail.com>
2024-05-18 04:52:52 +0200

    nav items finished

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -32,20 +32,6 @@ export default function TabLayout() {
           tabBarActiveTintColor: activeIconColor,
           title: 'Popis pitanja',
           tabBarIcon: ({ color }) => <TabBarIcon name="book" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="align-justify"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
         }}
       />
       <Tabs.Screen
